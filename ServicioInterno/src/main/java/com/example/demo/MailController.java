31e6d47f5bfcc9465dcd5729b601694f966cb26c
elenagarciamorato <e.garciapi@alumnos.urjc.es>
2019-03-20 23:23:18 +0100

    Corregido envio de objeto a la API

--- ServicioInterno/src/main/java/com/example/demo/MailController.java
+++ ServicioInterno/src/main/java/com/example/demo/MailController.java
@@ -1,10 +1,8 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sun.mail.smtp.SMTPTransport;
@@ -31,11 +29,11 @@ public class MailController {
 	
 	
 	@SuppressWarnings("restriction")
-	@GetMapping(value = "/correo/{nombre}/{email}/{rol}")
-	public ResponseEntity<Boolean> sendMail(@PathVariable String nombre, @PathVariable String email, @PathVariable String rol) {
+	@PostMapping(value = "/correo/")
+	public void sendMail(@RequestBody Mensaje user) {
 		
 		System.out.println("Datos recibidos!");
-		System.out.println("Nombre: " + nombre + "  Email: " + email + "  Rol: " + rol);
+		System.out.println("Nombre: " + user.getNombre() + "  Email: " + user.getEmail() + "  Rol: " + user.getRol());
 		
 		try {
 			Security.addProvider(new com.sun.net.ssl.internal.ssl.Provider());
@@ -57,14 +55,14 @@ public class MailController {
 			
 			
 			msg.setFrom(new InternetAddress(username));
-			msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email, false));
+			msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(user.getEmail(), false));
 			msg.setSubject("Bienvenido");
 			
 			//Se envia un mensaje diferente dependiendo si te registras como entrenador o deportista
-			if(rol.equals("deportista")){
-				msg.setText(" Hola " + nombre.toUpperCase() + ", \n \n Bienvenido a HealthSport, la herramienta que te permitirá controlar tus progresos y los de tu equipo! \n Al registrarte como deportista, un entrenador te seleccionará proximamente y pasarás a formar parte de su equipo. \n \n Quedamos a tu disposición para lo que necesites. \n \n Gracias por confiar en nosotros. \n \n El equipo de HealthSport", "utf-8");
+			if(user.getRol().equals("ROLE_DEPORTISTA")){
+				msg.setText(" Hola " + user.getNombre().toUpperCase() + ", \n \n Bienvenido a HealthSport, la herramienta que te permitirá controlar tus progresos y los de tu equipo! \n Al registrarte como deportista, un entrenador te seleccionará proximamente y pasarás a formar parte de su equipo. \n \n Quedamos a tu disposición para lo que necesites. \n \n Gracias por confiar en nosotros. \n \n El equipo de HealthSport", "utf-8");
 			}else{
-				msg.setText(" Hola " + nombre.toUpperCase() + ", \n \n Bienvenido a HealthSport, la herramienta que te permitirá controlar tus progresos y los de tu equipo! \n Al registrarte como entrenador, obtienes acceso a una lista de jugadores que puedes seleccionar para que formen parte de tu equipo. \n \n Quedamos a tu disposición para lo que necesites. \n \n Gracias por confiar en nosotros. \n \n El equipo de HealthSport", "utf-8");
+				msg.setText(" Hola " + user.getNombre().toUpperCase() + ", \n \n Bienvenido a HealthSport, la herramienta que te permitirá controlar tus progresos y los de tu equipo! \n Al registrarte como entrenador, obtienes acceso a una lista de jugadores que puedes seleccionar para que formen parte de tu equipo. \n \n Quedamos a tu disposición para lo que necesites. \n \n Gracias por confiar en nosotros. \n \n El equipo de HealthSport", "utf-8");
 			}
 			msg.setSentDate(new Date());
 
@@ -79,7 +77,6 @@ public class MailController {
 			System.out.println(ex);
 		}	
 		
-		return new ResponseEntity<Boolean>(true,HttpStatus.OK);
 		
 	}
 	
